e1bed69844cf4a8e014f8835fb40306d2571e94a
notchioma <paullois829@gmail.com>
2022-04-07 11:31:28 +0100

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ]
 })
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/Chop.Africa/" : "/",
+};
