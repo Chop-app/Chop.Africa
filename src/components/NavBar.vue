2fd6f0c07da4b8bb323bed4eb7603c499b475128
notchioma <paullois829@gmail.com>
2022-04-19 08:26:11 +0100

    changed menu icon and edited the menu bar content

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,6 +11,7 @@
         Chop.africa</a
       >
       <button
+        style="border: none"
         class="navbar-toggler"
         type="button"
         data-bs-toggle="collapse"
@@ -19,21 +20,24 @@
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <i
+          class="fa-solid fa-burger fa-2x"
+          style="color: var(--brand-color)"
+        ></i>
       </button>
       <div
         class="collapse navbar-collapse justify-content-end"
         id="navbarTogglerDemo02"
       >
-        <ul class="navbar-nav mb-2">
+        <ul class="navbar-nav mb-2 space-ul">
           <li class="nav-item">
-            <a class="nav-link">Partner With Us</a>
+            <a class="nav-link space">Partner With Us</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link ms-3">Recipe Blog</a>
+            <a class="nav-link margin-left space">Recipe Blog</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link ms-3">Contact Us</a>
+            <a class="nav-link margin-left space">Contact Us</a>
           </li>
         </ul>
       </div>
@@ -54,16 +58,33 @@ export default {};
 }
 
 .nav-link {
-  color: #2d4059 !important;
+  color: var(--brand-color) !important;
   font-weight: 450;
   font-size: 1.15rem;
 }
 .navbar-brand {
   font-weight: 600;
-  color: #2d4059 !important;
+  color: var(--brand-color) !important;
   font-size: 1.3rem;
   @media (max-width: 500px) {
     font-size: 1rem;
+  }
+}
+.margin-left {
+  margin-left: 20px;
+  @media (max-width: 992px) {
+    margin-left: 0;
+  }
+}
+.space-ul {
+  @media (max-width: 992px) {
+    margin: 20px;
+  }
+  .space {
+    cursor: pointer;
+    @media (max-width: 992px) {
+      padding: 10px 0 10px 30px;
+    }
   }
 }
 </style>
